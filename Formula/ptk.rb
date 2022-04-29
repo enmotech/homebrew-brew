ec1c27e34636fc2bf7fa5f386d698ed2cd5457c7
vimiix <i@vimiix.com>
2022-04-29 14:24:15 +0800

    fix:try to fix ptk version

--- Formula/ptk.rb
+++ Formula/ptk.rb
@@ -2,6 +2,7 @@ class Ptk < Formula
     desc "Easy to manage MogDB database"
     homepage "https://docs.mogdb.io/zh/ptk"
     url "https://cdn-mogdb.enmotech.com/ptk/ptk-latest.tar.gz"
+    version "latest"
     sha256 "d0a96741fb64701737f4cc7e30403cc56091e6dc46493b721d3b490b77b35013"
     license "MIT"
   
